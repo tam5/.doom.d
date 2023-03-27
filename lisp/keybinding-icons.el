68c17b38dc851d5d9826e595f2448221b9d79b67
tam5 <arimiller92@gmail.com>
2023-03-26 21:56:23 -0400

    stash

--- lisp/keybinding-icons.el
+++ lisp/keybinding-icons.el
@@ -10,14 +10,13 @@
 
 (defvar my/keybinding-icon-str-max-icons 8)
 
+(defvar my/assumed-max-keybinding-chords 5)
+
 (defun my/make-keybinding-icon (key)
   ""
   (svg-lib-tag key nil
    :background (doom-lighten (face-attribute 'default :background) 0.1)
    :foreground (doom-lighten (face-attribute 'default :background) 0.6)
-   ;; :height my/keybinding-icon-scale-factor
-   ;; :font-size (round (* (face-attribute 'default :height)
-   ;;                      (* my/keybinding-icon-scale-factor .1)))
    :font-size 13
    :font-weight 'bold
    :margin .5 :padding .5 :radius 3 :stroke 0))
@@ -26,7 +25,7 @@
   ""
   (let* ((description (key-description keybinding))
          (binding-parts (delq "" (split-string description " ")))
-         (filler-width (- 5 (length binding-parts))))
+         (filler-width (- my/assumed-max-keybinding-chords (length binding-parts))))
     (concat (mapconcat (lambda (part)
                            (propertize part 'display (my/make-keybinding-icon part)))
                        binding-parts)
@@ -39,16 +38,6 @@
              (key (and (commandp sym) (where-is-internal sym nil 'first-only))))
     (propertize (my/map-to-keybinding-icon-str key) 'face 'marginalia-key)))
 
-;; (defun my/marginalia-annotate-command (cand)
-;;   "Annotate command CAND with its documentation string.
-;; Similar to `marginalia-annotate-symbol', but does not show symbol class."
-;;   (when-let (sym (intern-soft cand))
-;;     (let* ((binding-str (marginalia-annotate-binding cand))
-;;            (padding-str ""))
-;;            ;; (padding-str (make-string (- 16 (length binding-str)) ?\s)))
-;;            (marginalia--fields
-;;             ((concat binding-str padding-str (marginalia--documentation (marginalia--function-doc sym))))))))
-
 (defun my/marginalia-annotate-command (cand)
   ""
   (when-let (sym (intern-soft cand))
@@ -56,4 +45,8 @@
      ((marginalia-annotate-binding cand) :truncate 0.165)
      ((marginalia--function-doc sym) :truncate 1.0 :face 'marginalia-documentation))))
 
-;; (key-description (kbd "SPC x"))  ; "8-x C-f"
+;; left TODO
+;; - apply the reset for the heights
+;; - make the mapper for the icons
+;; - make a cache for the svgs
+;; - rename to proper convention, maybe extract to package
